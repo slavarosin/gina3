19379d633b974f8d79ec91f1c6d9f6dcb89456f7
Vjatseslav Rosin <slavarosin@gmail.com>
2014-09-28 22:36:03 +0200

    Mongeez library removed as something not needed.

--- src/main/java/com/gina/config/DatabaseConfiguration.java
+++ src/main/java/com/gina/config/DatabaseConfiguration.java
@@ -1,47 +1,41 @@
 package com.gina.config;
 
+import javax.inject.Inject;
 
-import com.mongodb.Mongo;
-import org.mongeez.Mongeez;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
-import org.springframework.context.ApplicationContextException;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import javax.inject.Inject;
+
+import com.mongodb.Mongo;
 
 @Configuration
 @EnableMongoRepositories("com.gina.repository")
 @Import(value = MongoAutoConfiguration.class)
 @EnableMongoAuditing(auditorAwareRef = "springSecurityAuditorAware")
 public class DatabaseConfiguration implements EnvironmentAware {
-
-    private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
-
     private RelaxedPropertyResolver propertyResolver;
 
     private Environment environment;
 
     @Inject
     private Mongo mongo;
-    
 
     @Override
     public void setEnvironment(Environment environment) {
         this.environment = environment;
         this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.data.mongodb.");
     }
+
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener() {
         return new ValidatingMongoEventListener(validator());
@@ -54,21 +48,7 @@ public class DatabaseConfiguration implements EnvironmentAware {
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongo, propertyResolver.getProperty("databaseName"));
+        return new MongoTemplate(mongo, propertyResolver.getProperty("databaseName"), new UserCredentials(propertyResolver.getProperty("user"),
+                propertyResolver.getProperty("pass")));
     }
-
-    @Bean
-    public Mongeez mongeez() {
-        log.debug("Configuring Mongeez");
-        Mongeez mongeez = new Mongeez();
-
-        mongeez.setFile(new ClassPathResource("/config/mongeez/master.xml"));
-        mongeez.setMongo(mongo);
-        mongeez.setDbName(propertyResolver.getProperty("databaseName"));
-        mongeez.process();
-
-        return mongeez;
-    }
-    
 }
-
